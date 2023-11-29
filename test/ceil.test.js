2aca0e99e709aa3fc52eb288317bd0c1ac0d2020
Juho Kallunki <juho.kallunki@tuni.fi>
2023-11-29 13:48:55 +0200

    test commit

--- test/ceil.test.js
+++ test/ceil.test.js
@@ -2,7 +2,7 @@
 import ceil from '../src/ceil';
 
 test('decimal is rounded upwards without specifying precision', () => {
-    expect(ceil(4.0324))
+    expect(ceil(4.0325))
         .toBe(5)
         .toBeGreaterThan(4)
         .toBeLessThan(6);
