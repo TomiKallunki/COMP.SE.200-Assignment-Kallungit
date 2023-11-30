4e5c75b1a4c004d9ad5beb16b11e8d1807eb0556
Juho Kallunki <juho.kallunki@tuni.fi>
2023-11-30 17:03:17 +0200

    add eq tests

--- test/eq.test.js
+++ test/eq.test.js
@@ -4,6 +4,44 @@ import eq from '../src/eq';
 test('number 1 is equal to itself', () => {
     expect(eq(1, 1)).toBeTruthy();
 });
-test('number 1 is equal to 2', () => {
+test('number 1 is not equal to 2', () => {
     expect(eq(1, 2)).toBeFalsy();
+});
+test('number -3 is not equal to 3', () => {
+    expect(eq(-3, 3)).toBeFalsy();
+});
+test('number -1569 is equal to -1569', () => {
+    expect(eq(-1569, -1569)).toBeTruthy();
+});
+test('long decimal is the same', () => {
+    expect(eq(1.2935920350293592032985328523893258925832958223859, 1.2935920350293592032985328523893258925832958223859)).toBeTruthy();
+});
+test('string is equal to itself', () => {
+    expect(eq("test", "test")).toBeTruthy();
+});
+test('string is not equal to corresponding number', () => {
+    expect(eq("5", 5)).toBeFalsy();
+});
+test('character is the same as the character', () => {
+    expect(eq('y', 'y')).toBeTruthy();
+});
+test('character is not the same as object of that character', () => {
+    expect(eq('x', Object('x'))).toBeFalsy();
+});
+test('NaN is equal to NaN', () => {
+    expect(eq(NaN, NaN)).toBeTruthy();
+});
+test('different objects with same content are not equal', () => {
+    var x = { 'b': 2 }
+    var y = { 'b': 2 }
+    expect(eq(x, y)).toBeFalsy();
+});
+test('NaN is not equal to null', () => {
+    expect(eq(NaN, null)).toBeFalsy();
+});
+test('0 is equal to -0', () => {
+    expect(eq(0, -0)).toBeTruthy();
+});
+test('false is not equal to undefined', () => {
+    expect(eq(false, undefined)).toBeFalsy();
 });
