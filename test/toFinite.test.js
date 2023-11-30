be4285a4b9a6a633ce8cc2d00214ed9662f8c5ef
Juho Kallunki <juho.kallunki@tuni.fi>
2023-11-30 17:13:11 +0200

    add toFinite tests

--- test/toFinite.test.js
+++ test/toFinite.test.js
@@ -1,6 +1,27 @@
 
 import toFinite from '../src/toFinite';
 
-test('temp', () => {
-    expect(1).toBe(1);
+test('infinity to finite', () => {
+    expect(toFinite(Infinity)).toBe(1.7976931348623157e+308);
+});
+test('1.7976931348623157e+309 to finite', () => {
+    expect(toFinite(1.7976931348623157e+309)).toBe(1.7976931348623157e+308);
+});
+test('minimum value to finite', () => {
+    expect(toFinite(Number.MIN_VALUE)).toBe(5e-324);
+});
+test('regular positive number to finite', () => {
+    expect(toFinite(5522)).toBe(5522);
+});
+test('regular negative number to finite', () => {
+    expect(toFinite(-725)).toBe(-725);
+});
+test('decimal to finite', () => {
+    expect(toFinite(1.2567)).toBe(1.2567);
+});
+test('string number to finite', () => {
+    expect(toFinite('790000')).toBe(790000);
+});
+test('parameter is not a number', () => {
+    expect(toFinite("abc")).toBeFalsy();
 });
